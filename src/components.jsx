eded2576a974f8ea23d4447b215d895d2a8fa0ee
sukhress warun <sukhresswarun@gmail.com>
2024-03-18 20:43:46 +0530

    last 2 tables

--- src/components.jsx
+++ src/components.jsx
@@ -7,7 +7,7 @@ const TableComponents =  function ({title, params}){
     return(<>
         <h2 className={"text-light"}>{title}</h2>
 
-        <table className="table table-dark table-hover">
+        <table className="table table-dark table-hover mb-5">
             <thead>
             <tr>
                 <th scope="col">Sl No</th>
@@ -20,10 +20,10 @@ const TableComponents =  function ({title, params}){
                 return(
                     <tr>
                         <td scope="row">{index+1}</td>
-                        <td>{param.name}</td>
+                        <td style={{width: '300px'}}>{param.name}</td>
                         <td>
                                 {/*<input type="text" name={"params.name"} />*/}
-                            <div className="input-group mb-3">
+                            <div className="input-group">
                                 {
                                     parse(param.input)
                                 }
@@ -43,21 +43,21 @@ export default function components() {
 
     const tableData = {
         staticInput: [
-            {name: "No of Stokes", unit: "n", input:'<select class="" id="exampleFormControlSelect1" style="width:200px; padding-left: 20px;"> <option>2</option><option>4</option></select>'},
-            {name: "Fuel Correction Factor", unit: "n", input: `<input type="number" name={"params.name"} style="width:200px; padding-left: 20px;" step="0.05" min="0.05" max="1"/>`},
-            {name: "Nozzle Size", unit: "mm", input: `<input type="number" name={"params.name"} style="width:200px; padding-left: 20px;" step="0.1" min="1.0" max="2.5"/>`},
-            {name: "No of Cylinders", unit: "n", input: `<select class="" id="exampleFormControlSelect1" style="width:200px; padding-left: 20px;"> <option>1</option></select>`},
+            {name: "No of Stokes", unit: "n", input:'<select class="" id="exampleFormControlSelect1" style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;"> <option>2</option><option>4</option></select>'},
+            {name: "Fuel Correction Factor", unit: "n", input: `<input type="number" name={"params.name"} style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" step="0.05" min="0.05" max="1"/>`},
+            {name: "Nozzle Size", unit: "mm", input: `<input type="number" name={"params.name"} style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" step="0.1" min="1.0" max="2.5"/>`},
+            {name: "No of Cylinders", unit: "n", input: `<select class="" id="exampleFormControlSelect1" style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;"> <option>1</option></select>`},
         ],
 
         dynamicInput: [
-            {name: "Engine Speed", unit: "RPM", value: 0},
-            {name: "Manifold Absolute Pressure", unit: "Bar" , value: 0},
-            {name: "Exhaust Temperatire", unit: "Deg C", value: 0},
-            {name: "Gas Temperature", unit: "Deg C", value: 0},
+            {name: "Engine Speed", unit: "RPM", input:'<input value="0" readonly style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" />'},
+            {name: "Manifold Absolute Pressure", unit: "Bar" , input:'<input value="0" readonly style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" />'},
+            {name: "Exhaust Temperatire", unit: "Deg C", input:'<input value="0" readonly style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" />'},
+            {name: "Gas Temperature", unit: "Deg C", input:'<input value="0" readonly style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" />'},
         ],
         dynamicOutput: [
-            {name: "PWM H", unit: "ms"},
-            {name: "PWM L", unit: "ms"},
+            {name: "PWM H", unit: "ms", input:'<input value="0" readonly style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" />'},
+            {name: "PWM L", unit: "ms", input:'<input value="0" readonly style="width:200px; padding-left: 20px; border-radius: 5px 0 0 5px;" />'},
         ],
 
     }
@@ -66,7 +66,7 @@ export default function components() {
 
 
     return(<>
-        <div className="container-fluid">
+        <div className="container-fluid" style={{padding: '2%'}}>
             <div className="row">
                 <div className="col">
                     <div className="row">
@@ -74,16 +74,16 @@ export default function components() {
                             <TableComponents title={"Static Input"} params={tableData.staticInput}/>
                         </div>
                     </div>
-                    {/*<div className="row">*/}
-                    {/*    <div className="col">*/}
-                    {/*        <TableComponents title={"Dynamic Input"} params={tableData.dynamicInput}/>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*<div className="row">*/}
-                    {/*    <div className="col">*/}
-                    {/*        <TableComponents title={"Dynamic Output"} params={tableData.dynamicOutput}/>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div className="row">
+                       <div className="col">
+                           <TableComponents title={"Dynamic Input"} params={tableData.dynamicInput}/>
+                       </div>
+                    </div>
+                    <div className="row">
+                       <div className="col">
+                           <TableComponents title={"Dynamic Output"} params={tableData.dynamicOutput}/>
+                       </div>
+                    </div>
                 </div>
                 <div className="col">
                     <div className="row">
